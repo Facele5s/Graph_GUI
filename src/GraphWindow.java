9aa49df61de9a2829499de0fa80f5b157f6e7fdf
Facele5s <otherworldly.elban@gmail.com>
2023-08-10 01:38:13 +0300

    Added screen points calculation

--- src/GraphWindow.java
+++ src/GraphWindow.java
@@ -1,7 +1,14 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GraphWindow {
+    BufferedImage img;
+
     private int width;
     private int height;
 
@@ -17,6 +24,7 @@ public class GraphWindow {
     public GraphWindow(Graph graph, int width, int height) {
         this.width = width;
         this.height = height;
+        img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
         updateGraphParams(graph);
     }
@@ -29,10 +37,47 @@ public class GraphWindow {
         this.points = graph.getRealPoints();
     }
 
-
     public void calculateScreenPoints() {
+        double resX = (x_max - x_min) / (width - 1);  // Resolution: steps per pixel
+        double resY = (y_max - y_min) / (height * 0.9 - 1); // 10% of vertical space must be free
 
+        int x;
+        int y;
+
+        for(Graph.Point p: points) {
+            x = (int) (p.getX() / resX + (width) / 2);
+
+            if(p.isReal()) {
+                y = (int) (-p.getY() / resY + height / 2);
+                graphPoints.add(new ScreenPoint(x, y, false));
+            } else {
+                graphPoints.add(new ScreenPoint(x, true));
+            }
+        }
+
+        /*Graphics2D g = img.createGraphics();
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, width, height);
+        g.setColor(Color.WHITE);
+
+        for(ScreenPoint p: graphPoints) {
+            try {
+                if(!p.isEmpty()) img.setRGB(p.getX(), p.getY(), Color.WHITE.getRGB());
+            } catch (ArrayIndexOutOfBoundsException e) {
+                //
+            }
+
+            //img.setRGB(p.getX() / 2, 0, Color.WHITE.getRGB());
+        }
+
+        File f = new File("imgg.png");
+        try {
+            ImageIO.write(img, "png", f);
+        } catch (IOException e) {
+            //
+        }*/
     }
+
     public void drawGraph() {
 
     }
@@ -44,10 +89,18 @@ public class GraphWindow {
     static class ScreenPoint {
         private int x;
         private int y;
+        private boolean empty;
 
-        public ScreenPoint(int x, int y) {
+        public ScreenPoint(int x, int y, boolean empty) {
             this.x = x;
             this.y = y;
+            this.empty = empty;
+        }
+
+        public ScreenPoint(int x, boolean empty) {
+            this.x = x;
+            this.y = 0;
+            this.empty = empty;
         }
 
         public int getX() {
@@ -56,6 +109,15 @@ public class GraphWindow {
 
         public int getY() {
             return y;
+        }
+
+        public boolean isEmpty() {
+            return empty;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("ScreenPoint[x: %d, y: %d, empty: %s]", x, y, empty);
         }
     }
 }
